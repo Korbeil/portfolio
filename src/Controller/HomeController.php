bcd8cc6a3f5be2193e7f2d920382857e878c2639
Baptiste Leduc <baptiste.leduc@mon-avocat.fr>
2018-01-30 20:32:48 +0100

    home routing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,14 +8,13 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function index()
     {
