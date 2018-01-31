ee3ddde5db344ba72901e7f9ea13274205911e00
Baptiste Leduc <baptiste.leduc@mon-avocat.fr>
2018-01-31 22:09:38 +0100

    implementing contact & mail methods

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,17 +7,34 @@
  */
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ContactController extends Controller
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact", methods={"GET"})
      */
     public function index()
     {
         return $this->render('contact.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact_send", methods={"POST"})
+     * @param Request $request
+     */
+    public function send(Request $request)
+    {
+        $firstName  = $request->get('firstName');
+        $lastName   = $request->get('lastName');
+        $email      = $request->get('email');
+        $message    = $request->get('message');
+
+        $headers    = 'From: "' .$firstName. ' ' .$lastName. '"<' .$email. '>'."\n";
+        $headers   .= 'Content-Type: text/html; charset="utf-8"';
+        mail(getenv('MAILTO'), '[' .date('Y-m-d H:i:s'). '] mealtime.io - ContactForm', $message, $headers);
     }
 
 }
