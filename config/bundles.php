c7c7e6f30d3ebec78756f25a23b9989ebac32f25
Baptiste Leduc <baptiste.leduc@mon-avocat.fr>
2018-03-14 15:38:35 +0100

    adding CKEditor & implementing it for EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Ivory\CKEditorBundle\IvoryCKEditorBundle::class => ['all' => true],
 ];
